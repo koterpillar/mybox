36ec0a7b781ba68c112056e4cc93cacab48cedfa
Alexey Kotlyarov <a@koterpillar.com>
2022-08-15 13:26:59 +1000

    Test dot links

--- tests/package/test_links.py
+++ tests/package/test_links.py
@@ -1,16 +1,40 @@
 import os
+from abc import ABCMeta, abstractmethod
+from typing import Any
 
 from .base import PackageTestBase
 
 
-class TestLinks(PackageTestBase):
-    constructor_args = {
-        "links": f"{os.path.dirname(__file__)}/test_links_content",
-        "dest": "config",
-    }
+class LinksTestBase(PackageTestBase, metaclass=ABCMeta):
+    @property
+    def constructor_args(self) -> dict[str, Any]:
+        return {
+            "links": f"{os.path.dirname(__file__)}/test_links_content",
+            "dest": "config",
+        }
 
     @property
     def check_installed_command(self) -> list[str]:
-        return ["cat", f"{os.environ['MYBOX_HOME']}/config/myfile"]
+        return ["cat", f"{os.environ['MYBOX_HOME']}/{self.destination_file}"]
 
     check_installed_output = "Linked file"
+
+    @property
+    @abstractmethod
+    def destination_file(self) -> str:
+        pass
+
+
+class TestLinks(LinksTestBase):
+    destination_file = "config/myfile"
+
+
+class TestDotLinks(LinksTestBase):
+    @property
+    def constructor_args(self) -> dict[str, Any]:
+        return {
+            **super().constructor_args,
+            "dot": True,
+        }
+
+    destination_file = "config/.myfile"
