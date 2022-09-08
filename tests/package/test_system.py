1589988e4548e9b6fda5ce37c97446433a04605a
Alexey Kotlyarov <a@koterpillar.com>
2022-09-08 11:04:23 +1000

    Tap cask-fonts for tests

--- tests/package/test_system.py
+++ tests/package/test_system.py
@@ -1,15 +1,19 @@
 import pytest
 
 from mybox.package.system import Brew
-from mybox.utils import CURRENT_OS
+from mybox.utils import CURRENT_OS, run
 
 
 class TestBrew:
-    @pytest.fixture(autouse=True)
+    @pytest.fixture
     def brew(self):
         if CURRENT_OS != "darwin":
             pytest.skip("brew is only available on macOS")
         return Brew()
+
+    @pytest.fixture(autouse=True)
+    def brew_tap_fonts(self, brew):  # pylint:disable=unused-argument
+        run("brew", "tap", "homebrew/cask-fonts")
 
     def test_formula_version(self, brew):
         assert "8.10" <= brew.latest_version("ghc") <= "99"
@@ -23,3 +27,7 @@ class TestBrew:
     def test_not_existing_formula(self, brew):
         with pytest.raises(Exception, match="zzzzzzzz"):
             brew.latest_version("zzzzzzzz")
+
+    def test_non_tapped_cask(self, brew):
+        with pytest.raises(Exception, match="homebrew/cask-zzzzzzz/yyyyyyy"):
+            brew.latest_version("homebrew/cask-zzzzzzz/yyyyyyy")
