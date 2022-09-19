b49f882530862517ca33413f3a9a251e1d7a2f2e
Alexey Kotlyarov <a@koterpillar.com>
2022-09-19 17:32:14 +1000

    Implement cloning as root

--- mybox/package/clone.py
+++ mybox/package/clone.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 from ..fs import makedirs
-from ..utils import run, run_output
+from ..utils import run, run_ok, run_output
 from .destination import Destination
 
 
@@ -16,13 +16,22 @@ class Clone(Destination):
 
     @property
     def directory_exists(self) -> bool:
+        if self.root:
+            return run_ok("sudo", "test", "-d", str(self.destination))
         return self.destination.is_dir()
+
+    @property
+    def git_args(self) -> list[str]:
+        result = ["git", "-C", str(self.destination)]
+        if self.root:
+            result.insert(0, "sudo")
+        return result
 
     @property
     def local_version(self) -> Optional[str]:
         if not self.directory_exists:
             return None
-        return run_output("git", "rev-parse", "HEAD", cwd=self.destination)
+        return run_output(*self.git_args, "rev-parse", "HEAD")
 
     @property
     def remote(self):
@@ -32,13 +41,14 @@ class Clone(Destination):
         return run_output("git", "ls-remote", self.remote, "HEAD").split()[0]
 
     def install(self) -> None:
-        makedirs(self.destination.parent)
+        makedirs(self.destination.parent, sudo=self.root)
         if not self.directory_exists:
-            run("git", "clone", self.remote, str(self.destination))
-        run("git", "remote", "set-url", "origin", self.remote, cwd=self.destination)
-        run("git", "fetch", cwd=self.destination)
+            prefix = ["sudo"] if self.root else []
+            run(*prefix, "git", "clone", self.remote, str(self.destination))
+        run(*self.git_args, "remote", "set-url", "origin", self.remote)
+        run(*self.git_args, "fetch")
         default_branch = run_output(
-            "git", "rev-parse", "--abbrev-ref", "origin/HEAD", cwd=self.destination
+            *self.git_args, "rev-parse", "--abbrev-ref", "origin/HEAD"
         ).split("/")[1]
-        run("git", "switch", default_branch, cwd=self.destination)
-        run("git", "reset", "--hard", f"origin/{default_branch}", cwd=self.destination)
+        run(*self.git_args, "switch", default_branch)
+        run(*self.git_args, "reset", "--hard", f"origin/{default_branch}")
--- tests/package/test_clone.py
+++ tests/package/test_clone.py
@@ -1,5 +1,8 @@
 import os
+from pathlib import Path
 from typing import Any
+
+from mybox.utils import run
 
 from .base import PackageTestBase
 
@@ -13,10 +16,34 @@ class TestClone(PackageTestBase):
         }
 
     @property
+    def destination_dir(self) -> str:
+        return os.environ["MYBOX_HOME"]
+
+    @property
     def check_installed_command(self) -> list[str]:
         return [
             "cat",
-            f"{os.environ['MYBOX_HOME']}/oh-my-zsh/templates/zshrc.zsh-template",
+            f"{self.destination_dir}/oh-my-zsh/templates/zshrc.zsh-template",
         ]
 
     check_installed_output = "alias ohmyzsh"
+
+
+class TestRootClone(TestClone):
+    @property
+    def constructor_args(self) -> dict[str, Any]:
+        return super().constructor_args | {"root": True}
+
+    def test_installs(self):
+        try:
+            return super().test_installs()
+        finally:
+            run("sudo", "-i", "rm", "-rf", "oh-my-zsh")
+
+    @property
+    def destination_dir(self) -> str:
+        return str(Path("~root").expanduser())
+
+    @property
+    def check_installed_command(self) -> list[str]:
+        return ["sudo", *super().check_installed_command]
