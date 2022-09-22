90b2f133db7a46f085ee6c285a5c7c9f665ac8a9
Alexey Kotlyarov <a@koterpillar.com>
2022-09-22 23:59:26 +1000

    Switch pip and pipx to FS

--- mybox/package/pip.py
+++ mybox/package/pip.py
@@ -2,7 +2,6 @@ import json
 import subprocess
 from typing import Optional
 
-from ..utils import run, run_output
 from .pip_base import PipBasePackage
 
 
@@ -10,22 +9,22 @@ def pip_cmd(cmd: str, /, *args: str) -> list[str]:
     return ["python3", "-m", "pip", cmd, "--user", *args]
 
 
-def get_pip_versions() -> dict[str, str]:
-    packages = json.loads(
-        run_output(*pip_cmd("list", "--format", "json"), stderr=subprocess.DEVNULL)
-    )
-    return {package["name"]: package["version"] for package in packages}
-
-
 class PipPackage(PipBasePackage):
     def __init__(self, *, pip: str, **kwargs) -> None:
         self.package = pip
         super().__init__(**kwargs)
 
+    def get_pip_versions(self) -> dict[str, str]:
+        packages_json = self.fs.run_output(
+            *pip_cmd("list", "--format", "json"), stderr=subprocess.DEVNULL
+        )
+        packages = json.loads(packages_json)
+        return {package["name"]: package["version"] for package in packages}
+
     @property
     def local_version(self) -> Optional[str]:
-        return get_pip_versions().get(self.package)
+        return self.get_pip_versions().get(self.package)
 
     def install(self) -> None:
         cmd = "install" if self.local_version is None else "upgrade"
-        run(*pip_cmd(cmd, self.package))
+        self.fs.run(*pip_cmd(cmd, self.package))
--- mybox/package/pipx.py
+++ mybox/package/pipx.py
@@ -2,20 +2,9 @@ import json
 import subprocess
 from typing import Optional
 
-from ..utils import run, run_output
 from .pip_base import PipBasePackage
 
 PIPX = ["python3", "-m", "pipx"]
-
-
-def get_pipx_versions() -> dict[str, str]:
-    pipx_list = json.loads(
-        run_output(*PIPX, "list", "--json", stderr=subprocess.DEVNULL)
-    )
-    packages = (
-        item["metadata"]["main_package"] for item in pipx_list["venvs"].values()
-    )
-    return {package["package"]: package["package_version"] for package in packages}
 
 
 class PipxPackage(PipBasePackage):
@@ -23,10 +12,19 @@ class PipxPackage(PipBasePackage):
         super().__init__(**kwargs)
         self.package = pipx
 
+    def get_pipx_versions(self) -> dict[str, str]:
+        pipx_list = json.loads(
+            self.fs.run_output(*PIPX, "list", "--json", stderr=subprocess.DEVNULL)
+        )
+        packages = (
+            item["metadata"]["main_package"] for item in pipx_list["venvs"].values()
+        )
+        return {package["package"]: package["package_version"] for package in packages}
+
     @property
     def local_version(self) -> Optional[str]:
-        return get_pipx_versions().get(self.package)
+        return self.get_pipx_versions().get(self.package)
 
     def install(self) -> None:
         cmd = "install" if self.local_version is None else "upgrade"
-        run(*PIPX, cmd, self.package)
+        self.fs.run(*PIPX, cmd, self.package)
