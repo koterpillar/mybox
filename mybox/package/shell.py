a5e0c1da7a4ea16792c636299e04150618d96517
Alexey Kotlyarov <a@koterpillar.com>
2022-09-22 23:47:09 +1000

    Read shells via FS

--- mybox/package/shell.py
+++ mybox/package/shell.py
@@ -1,16 +1,12 @@
 import os
 import pwd
+from functools import cached_property
 from pathlib import Path
 from typing import Optional
 
 from .base import Package
 
-SHELLS_FILE = "/etc/shells"
-
-
-def get_all_shells() -> list[str]:
-    with open(SHELLS_FILE) as shells_file:
-        return [shell.strip() for shell in shells_file]
+SHELLS_FILE = Path("/etc/shells")
 
 
 class Shell(Package):
@@ -29,13 +25,17 @@ class Shell(Package):
     def local_version(self) -> Optional[str]:
         return pwd.getpwuid(os.getuid()).pw_shell
 
+    @cached_property
+    def all_shells(self) -> list[str]:
+        return self.fs.read_file(SHELLS_FILE).splitlines()
+
     def install(self) -> None:
         if not self.fs.is_file(self.shell):
             raise ValueError(f"{self.shell} does not exist.")
         if not self.fs.is_executable(self.shell):
             raise ValueError(f"{self.shell} is not executable.")
-        if str(self.shell) not in get_all_shells():
+        if str(self.shell) not in self.all_shells:
             self.fs.with_root(True).run(
-                "tee", "-a", SHELLS_FILE, input=str(self.shell).encode()
+                "tee", "-a", str(SHELLS_FILE), input=str(self.shell).encode()
             )
         self.fs.run("chsh", "-s", str(self.shell))
