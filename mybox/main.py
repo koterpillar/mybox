beae4c61ef84e5ead8b143db0e776852a1e45b21
Alexey Kotlyarov <a@koterpillar.com>
2022-10-23 16:37:25 +1100

    Use typed-argparse

--- mybox/main.py
+++ mybox/main.py
@@ -1,4 +1,7 @@
 import argparse
+from typing import Optional
+
+from typed_argparse import TypedArgs
 
 from .driver import LocalDriver
 from .package import load_packages
@@ -6,19 +9,24 @@ from .state import DB, DB_PATH
 from .utils import flatten, parallel_map_tqdm
 
 
-def parser() -> argparse.ArgumentParser:
+class Args(TypedArgs):
+    component: list[str]
+    sequential: bool
+
+
+def parse_args(args: Optional[list[str]] = None) -> Args:
     result = argparse.ArgumentParser(description="Install the environment")
     result.add_argument("component", nargs="*", help="The component to install")
     result.add_argument(
         "--sequential", action="store_true", help="Install packages one at a time"
     )
-    return result
+    return Args.from_argparse(result.parse_args(args))
 
 
 def main():
     db = DB(DB_PATH)
     driver = LocalDriver()
-    args = parser().parse_args()
+    args = parse_args()
     components: frozenset[str] = frozenset(args.component) | {"base"}
     packages = flatten(
         load_packages(component, db=db, driver=driver) for component in components
