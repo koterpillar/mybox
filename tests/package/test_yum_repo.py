28f3feaf0e356e04b21be6ad03d6da0869d2b515
Alexey Kotlyarov <a@koterpillar.com>
2022-11-12 21:10:02 +1100

    Test YUM repository

---
+++ tests/package/test_yum_repo.py
@@ -0,0 +1,27 @@
+import pytest
+
+from .base import PackageArgs, RootPackageTestBase
+
+
+class TestNodeSource(RootPackageTestBase):
+    affects_system = True
+    root = True
+
+    async def constructor_args(self) -> PackageArgs:
+        return {
+            "yum_name": "nodesource",
+            "yum_url": "https://rpm.nodesource.com/pub_19.x/fc/36/$basearch",
+            "gpg_key": "https://rpm.nodesource.com/pub/el/NODESOURCE-GPG-SIGNING-KEY-EL",
+        }
+
+    async def check_installed_command(self):
+        return ["yum", "info", "nodejs"]
+
+    check_installed_output = "19."
+
+    async def check_applicable(self) -> None:
+        await super().check_applicable()
+        if not (await self.driver.os()).switch_(
+            linux=lambda os: os.distribution == "fedora", macos=False
+        ):
+            pytest.skip("This test is only applicable on Fedora.")
