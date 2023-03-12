cdcf77494cb6417748f2177d8261759678d9131c
Alexey Kotlyarov <a@koterpillar.com>
2023-03-12 22:18:55 +1100

    Fix pip command to upgrade a package

--- mybox/package/pip.py
+++ mybox/package/pip.py
@@ -10,7 +10,7 @@ class PipPackage(PipBasePackage):
 
     async def get_all_versions(self) -> dict[str, str]:
         packages_json = await self.driver.run_output(
-            *self.cmd("list", "--format", "json"), silent=True
+            *PIP, "list", "--format", "json", silent=True
         )
 
         # https://github.com/pypa/pip/issues/11282
@@ -20,5 +20,6 @@ class PipPackage(PipBasePackage):
 
         return {package["name"].lower(): package["version"] for package in packages}
 
-    def cmd(self, cmd: str, /, *args: str) -> list[str]:
-        return [*PIP, cmd, "--user", *args]
+    async def install(self) -> None:
+        await self.driver.run(*PIP, "install", "--upgrade", "--user", self.package)
+        await super().install()
--- mybox/package/pipx.py
+++ mybox/package/pipx.py
@@ -2,6 +2,8 @@ import json
 
 from .pip_base import PipBasePackage
 
+PIPX = ["python3", "-m", "pipx"]
+
 
 class PipxPackage(PipBasePackage):
     def __init__(self, *, pipx: str, **kwargs) -> None:
@@ -10,12 +12,14 @@ class PipxPackage(PipBasePackage):
 
     async def get_all_versions(self) -> dict[str, str]:
         pipx_list = json.loads(
-            await self.driver.run_output(*self.cmd("list", "--json"), silent=True)
+            await self.driver.run_output(*PIPX, "list", "--json", silent=True)
         )
         packages = (
             item["metadata"]["main_package"] for item in pipx_list["venvs"].values()
         )
         return {package["package"]: package["package_version"] for package in packages}
 
-    def cmd(self, cmd: str, /, *args: str) -> list[str]:
-        return ["python3", "-m", "pipx", cmd, *args]
+    async def install(self) -> None:
+        cmd = "install" if await self.local_version() is None else "upgrade"
+        await self.driver.run(*PIPX, cmd, self.package)
+        await super().install()
