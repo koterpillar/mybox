da7e49789fc4f2e7a422935932fc896240a52df0
Alexey Kotlyarov <a@koterpillar.com>
2023-04-16 13:18:52 +1000

    Local version can be missing

--- src/Package.hs
+++ src/Package.hs
@@ -10,10 +10,10 @@ import           Driver
 class Package package where
   pkInstall :: Driver -> package -> IO ()
   pkRemoteVersion :: Driver -> package -> IO Text
-  pkLocalVersion :: Driver -> package -> IO Text
+  pkLocalVersion :: Driver -> package -> IO (Maybe Text)
 
 pkIsInstalled :: Package package => Driver -> package -> IO Bool
 pkIsInstalled driver package = do
   remote <- pkRemoteVersion driver package
   local <- pkLocalVersion driver package
-  pure $ remote == local
+  pure $ local == Just remote
