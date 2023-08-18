cb79c309916ed5fc2ed6feabf6260fe09588618f
Alexey Kotlyarov <a@koterpillar.com>
2023-08-19 09:43:45 +1000

    Track binaries installed by pipx

--- mybox/package/pipx.py
+++ mybox/package/pipx.py
@@ -1,6 +1,7 @@
 import json
 import re
-from typing import Optional, cast
+from pathlib import Path
+from typing import Any, Optional, cast
 
 import requests
 from pydantic import Field, validator
@@ -19,17 +20,22 @@ class PipxPackage(Tracked):
     def name(self) -> str:
         return self.package
 
-    async def get_all_versions(self) -> dict[str, str]:
+    async def get_metadata(self) -> Optional[dict[str, Any]]:
         pipx_list = json.loads(
             await self.driver.run_output("pipx", "list", "--json", silent=True)
         )
-        packages = (
-            item["metadata"]["main_package"] for item in pipx_list["venvs"].values()
-        )
-        return {package["package"]: package["package_version"] for package in packages}
+        venv = pipx_list["venvs"].get(self.package)
+        if venv:
+            return venv["metadata"]["main_package"]
+        else:
+            return None
 
     async def local_version(self) -> Optional[str]:
-        return (await self.get_all_versions()).get(self.package)
+        metadata = await self.get_metadata()
+        if metadata:
+            return metadata["package_version"]
+        else:
+            return None
 
     async def _get_pypi_version(self) -> Optional[str]:
         pypi_info = requests.get(f"https://pypi.org/pypi/{self.package}/json").json()
@@ -70,5 +76,12 @@ class PipxPackage(Tracked):
     async def install_tracked(self, *, tracker: Tracker) -> None:
         cmd = "install" if await self.local_version() is None else "upgrade"
         await self.driver.run("pipx", cmd, self.package)
+
         tracker.track(await self.driver.local() / "pipx" / "venvs" / self.package)
+        metadata = await self.get_metadata()
+        if metadata:
+            for bin_desc in metadata["app_paths"]:
+                bin_path = Path(bin_desc["__Path__"]).name
+                tracker.track(await self.driver.local() / "bin" / bin_path)
+
         await super().install_tracked(tracker=tracker)
