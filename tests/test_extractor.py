63698dc7a3e2a51b07595e6e02d8bfa24336f433
Alexey Kotlyarov <a@koterpillar.com>
2023-10-08 15:47:53 +1100

    Test unzip with strip

--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -1,7 +1,33 @@
+import tempfile
+from pathlib import Path
+
 import pytest
 
 from mybox.driver import LocalDriver
 from mybox.extractor import Tar, get_extractor
+
+
+@pytest.mark.trio
+async def test_unzip_strip():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        tmppath = Path(tmpdir)
+
+        (tmppath / "foo").mkdir()
+        (tmppath / "foo" / "bar").touch()
+        (tmppath / "foo" / "baz").touch()
+
+        archive = tmppath / "archive.zip"
+        await LocalDriver().run("zip", "-qq", archive, tmppath)
+
+        extractor = await get_extractor(str(archive), driver=LocalDriver())
+
+        with tempfile.TemporaryDirectory() as dest:
+            dest_path = Path(dest)
+            await extractor.extract(
+                archive=archive, target_directory=dest_path, strip=1
+            )
+
+            assert set(dest_path.iterdir()) == {dest_path / "bar", dest_path / "baz"}
 
 
 @pytest.mark.trio
