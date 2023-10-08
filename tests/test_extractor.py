3163c400b10c2e3e5be0c2c4bbb24ed2445f831b
Alexey Kotlyarov <a@koterpillar.com>
2023-10-08 17:11:21 +1100

    Test failures for strip

--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -1,6 +1,8 @@
 import shlex
 import tempfile
+from contextlib import asynccontextmanager
 from pathlib import Path
+from typing import AsyncIterator
 
 import pytest
 
@@ -9,16 +11,16 @@ from mybox.extractor import Tar, get_extractor
 from mybox.utils import run
 
 
-@pytest.mark.trio
-async def test_unzip_strip():
+@asynccontextmanager
+async def temporary_zip_archive(*paths: str) -> AsyncIterator[Path]:
     with tempfile.NamedTemporaryFile(suffix=".zip") as archive_file:
         archive = Path(archive_file.name)
         with tempfile.TemporaryDirectory() as tmpdir:
             tmppath = Path(tmpdir)
 
-            (tmppath / "foo").mkdir()
-            (tmppath / "foo" / "bar").touch()
-            (tmppath / "foo" / "baz").touch()
+            for element in paths:
+                (tmppath / element).parent.mkdir(parents=True, exist_ok=True)
+                (tmppath / element).touch()
 
             await run("rm", archive)
             await run(
@@ -27,15 +29,43 @@ async def test_unzip_strip():
                 f"cd {shlex.quote(str(tmppath))} && zip -qq -r {shlex.quote(str(archive))} .",
             )
 
-        extractor = await get_extractor(str(archive), driver=LocalDriver())
+            yield archive
 
-        with tempfile.TemporaryDirectory() as dest:
-            dest_path = Path(dest)
-            await extractor.extract(
-                archive=archive, target_directory=dest_path, strip=1
-            )
 
-            assert set(dest_path.iterdir()) == {dest_path / "bar", dest_path / "baz"}
+async def extract_file_names(archive: Path, *, strip: int = 0) -> set[str]:
+    extractor = await get_extractor(str(archive), driver=LocalDriver())
+
+    with tempfile.TemporaryDirectory() as dest:
+        dest_path = Path(dest)
+        await extractor.extract(
+            archive=archive, target_directory=dest_path, strip=strip
+        )
+
+        return set(
+            str(element.relative_to(dest_path)) for element in dest_path.iterdir()
+        )
+
+
+@pytest.mark.trio
+async def test_unzip_strip():
+    async with temporary_zip_archive("foo/bar", "foo/baz") as archive:
+        assert await extract_file_names(archive, strip=1) == {"bar", "baz"}
+
+
+@pytest.mark.trio
+async def test_unzip_strip_multiple_elements():
+    async with temporary_zip_archive("foo/bar", "foo/baz") as archive:
+        with pytest.raises(
+            ValueError, match="Expected exactly one item after extracting"
+        ):
+            await extract_file_names(archive, strip=2)
+
+
+@pytest.mark.trio
+async def test_unzip_strip_not_directory():
+    async with temporary_zip_archive("foo") as archive:
+        with pytest.raises(ValueError, match="Expected directory after extracting"):
+            await extract_file_names(archive, strip=1)
 
 
 @pytest.mark.trio
