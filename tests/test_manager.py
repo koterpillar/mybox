84e7842c06715d118d2468175d3c1696513918e3
Alexey Kotlyarov <a@koterpillar.com>
2024-02-12 21:12:54 +1100

    Refactor manager testing

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -1,4 +1,3 @@
-import tempfile
 from pathlib import Path
 from typing import Optional
 
@@ -12,7 +11,7 @@ from mybox.package.base import Package
 from mybox.package.github import GitHubPackage
 from mybox.package.manual_version import ManualVersion
 from mybox.package.root import Root
-from mybox.state import DB, INSTALLED_FILES, VERSIONS, InstalledFile, Version
+from mybox.state import DB, INSTALLED_FILES, VERSIONS, InstalledFile
 from mybox.tracker import Tracker
 from mybox.utils import RunArg, allow_singular_none
 
@@ -80,31 +79,61 @@ class TestManager:
     def package_names(cls, packages: list[Package]) -> list[str]:
         return [package.name for package in packages]
 
+    db: DB
+    driver: DummyDriver
+    manager: Manager
+
+    @pytest.fixture(autouse=True)
+    def make_db(self) -> None:
+        self.db = DB.temporary()
+
+    @pytest.fixture(autouse=True)
+    def make_driver(self) -> None:
+        self.driver = DummyDriver()
+
+    @pytest.fixture(autouse=True)
+    def make_manager(self, tmp_path: Path) -> None:
+        self.manager = Manager(db=self.db, driver=self.driver, component_path=tmp_path)
+
+    def write_component(self, name: str, *packages: dict) -> None:
+        with open(self.manager.component_path / f"{name}.yaml", "w") as out:
+            yaml.dump(list(packages), out, indent=4)
+
+    def make_package(
+        self,
+        name: str,
+        *,
+        files: Optional[list[str]] = None,
+        root: bool = False,
+        version: str = "1",
+        error: Optional[Exception] = None,
+    ) -> DummyPackage:
+        return DummyPackage(
+            db=self.db,
+            driver=self.driver,
+            name=name,
+            files=files or [],
+            root=root,
+            version=version,
+            error=error,
+        )
+
+    def installed_files(self) -> set[InstalledFile]:
+        return set(INSTALLED_FILES(self.db).find())
+
+    def versions(self) -> dict[str, str]:
+        return {name: version.version for name, version in VERSIONS(self.db).find_ids()}
+
     @pytest.mark.trio
     async def test_tracks_packages_and_files(self):
-        db = DB.temporary()
-        driver = DummyDriver()
-
-        def make_package(
-            name: str, *, files: list[str], root: bool = False, version: str = "1"
-        ) -> DummyPackage:
-            return DummyPackage(
-                db=db, driver=driver, name=name, files=files, root=root, version=version
-            )
-
-        installed_files = INSTALLED_FILES(db)
-        versions = VERSIONS(db)
-
-        manager = Manager(db=db, driver=driver, component_path=Path("/dev/null"))
-
         # Install packages onto an empty system; they install a shared file
         # and some files unique for each
-        result1 = await manager.install_packages(
+        result1 = await self.manager.install_packages(
             [
-                make_package("foo", files=["/shared", "/foo"]),
-                make_package("bar", files=["/shared", "/bar"]),
-                make_package("baz", files=["/baz"]),
-                make_package("quux", files=["/quux"], root=True),
+                self.make_package("foo", files=["/shared", "/foo"]),
+                self.make_package("bar", files=["/shared", "/bar"]),
+                self.make_package("baz", files=["/baz"]),
+                self.make_package("quux", files=["/quux"], root=True),
             ]
         )
         assert not result1.failed
@@ -118,89 +147,66 @@ class TestManager:
             InstalledFile(path="/baz", package="baz", root=False),
             InstalledFile(path="/quux", package="quux", root=True),
         }
-        assert set(installed_files.find()) == expected_files
+        assert self.installed_files() == expected_files
 
         # Install an updated version for one package, keep one and remove others
-        driver.reset()
-        result2 = await manager.install_packages(
+        self.driver.reset()
+        result2 = await self.manager.install_packages(
             [
-                make_package(
+                self.make_package(
                     "foo",
                     files=["/shared", "/foo", "/foo2"],
                     version="2",
                 ),
-                make_package("baz", files=["/baz"]),
+                self.make_package("baz", files=["/baz"]),
             ]
         )
         assert not result2.failed
         assert self.package_names(result2.installed) == ["foo"]
 
         # Check package versions: foo should be updated, bar should be removed
-        assert set(versions.find_ids()) == {
-            ("foo", Version(version="2")),
-            ("baz", Version(version="1")),
-        }
+        assert self.versions() == {"foo": "2", "baz": "1"}
 
         # Check installed files: everything from foo should be installed,
         # everything from bar removed, shared files left alone
-        assert set(installed_files.find()) == {
+        assert self.installed_files() == {
             InstalledFile(path="/shared", package="foo", root=False),
             InstalledFile(path="/shared", package="bar", root=False),
             InstalledFile(path="/foo", package="foo", root=False),
             InstalledFile(path="/foo2", package="foo", root=False),
             InstalledFile(path="/baz", package="baz", root=False),
         }
-        assert ["rm", "-r", "-f", "/bar"] in driver.commands
-        assert ["rm", "-r", "-f", "/shared"] not in driver.commands
+        assert ["rm", "-r", "-f", "/bar"] in self.driver.commands
+        assert ["rm", "-r", "-f", "/shared"] not in self.driver.commands
 
         # File installed with root should be removed with root
-        assert ["sudo", "rm", "-r", "-f", "/quux"] in driver.commands
+        assert ["sudo", "rm", "-r", "-f", "/quux"] in self.driver.commands
 
     @pytest.mark.trio
     async def test_collects_errors(self):
-        db = DB.temporary()
-
-        driver = DummyDriver()
-        manager = Manager(db=db, driver=driver, component_path=Path("/dev/null"))
-
-        result = await manager.install_packages(
+        result = await self.manager.install_packages(
             [
-                DummyPackage(db=db, driver=driver, name="good1"),
-                DummyPackage(db=db, driver=driver, name="good2"),
-                DummyPackage(
-                    db=db, driver=driver, name="bad1", error=Exception("exc1")
-                ),
-                DummyPackage(
-                    db=db, driver=driver, name="bad2", error=Exception("exc2")
-                ),
+                self.make_package(name="good1"),
+                self.make_package(name="good2"),
+                self.make_package(name="bad1", error=Exception("exc1")),
+                self.make_package(name="bad2", error=Exception("exc2")),
             ]
         )
 
         assert self.package_names(result.installed) == ["good1", "good2"]
-        assert [f"{package.name}: {error}" for package, error in result.failed] == [
-            "bad1: exc1",
-            "bad2: exc2",
-        ]
+        assert {package.name: str(error) for package, error in result.failed} == {
+            "bad1": "exc1",
+            "bad2": "exc2",
+        }
 
     def test_parses_packages(self):
-        with tempfile.TemporaryDirectory() as tmpdir:
-            tmppath = Path(tmpdir)
+        self.write_component("one", {"repo": "asdf/asdf", "binary": ["asdf"]})
+        self.write_component("two", {"name": "foo"})
 
-            def write_component(name: str, *packages: dict) -> None:
-                with open(tmppath / f"{name}.yaml", "w") as out:
-                    yaml.dump(list(packages), out, indent=4)
+        packages = self.manager.load_components(frozenset(["one"]))
 
-            write_component("one", {"repo": "asdf/asdf", "binary": ["asdf"]})
-            write_component("two", {"name": "foo"})
-
-            db = DB.temporary()
-            driver = DummyDriver()
-            manager = Manager(db=db, driver=driver, component_path=tmppath)
-
-            packages = manager.load_components({"one"})
-
-            assert len(packages) == 1
-            package = packages[0]
-            assert isinstance(package, GitHubPackage)
-            assert package.repo == "asdf/asdf"
-            assert package.binaries == ["asdf"]
+        assert len(packages) == 1
+        package = packages[0]
+        assert isinstance(package, GitHubPackage)
+        assert package.repo == "asdf/asdf"
+        assert package.binaries == ["asdf"]
