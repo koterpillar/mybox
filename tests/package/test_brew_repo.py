67ec7a390557f92486e49ce6f79d7ce04c480f07
Alexey Kotlyarov <a@koterpillar.com>
2024-05-03 21:06:12 +1000

    chore: Fix brew repo test

--- tests/package/test_brew_repo.py
+++ tests/package/test_brew_repo.py
@@ -3,20 +3,21 @@ import pytest
 from .base import PackageArgs, PackageTestBase
 
 
-class TestCaskVersions(PackageTestBase):
+class TestBrewRepo(PackageTestBase):
     affects_system = True
 
     async def constructor_args(self) -> PackageArgs:
+        # Chosen from https://docs.brew.sh/Interesting-Taps-and-Forks
         return {
-            "brew_tap": "homebrew/cask-versions",
+            "brew_tap": "denji/nginx",
         }
 
     async def check_installed_command(self):
-        return ["brew", "info", "iterm2-beta"]
+        return ["brew", "info", "nginx-full"]
 
-    check_installed_output = "iterm2.com"
+    check_installed_output = "HTTP(S) server"
 
     async def check_applicable(self) -> None:
         await super().check_applicable()
         if not (await self.driver.os()).switch(linux=False, macos=True):
-            pytest.skip("This test is only applicable on Fedora.")
+            pytest.skip("This test is only applicable on macOS.")
