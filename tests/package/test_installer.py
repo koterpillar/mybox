ae6d11057186a0115d4e366c3d0339055f2b14f0
Alexey Kotlyarov <a@koterpillar.com>
2024-05-17 12:55:52 +1000

    chore: Remove fonts tap test

--- tests/package/test_installer.py
+++ tests/package/test_installer.py
@@ -1,7 +1,6 @@
 import pytest
 
 from mybox.package.installer import DNF, Apt, Brew
-from mybox.utils import run
 
 from ..package.driver import TestDriver
 
@@ -15,10 +14,6 @@ class TestBrew:
         )
         return Brew(make_driver)
 
-    @pytest.fixture
-    async def brew_tap_fonts(self, brew):  # pylint:disable=unused-argument
-        await run("brew", "tap", "homebrew/cask-fonts")
-
     @pytest.mark.trio
     async def test_formula_version(self, brew: Brew):
         assert "8.10" <= await brew.latest_version("ghc") <= "99"
@@ -26,12 +21,6 @@ class TestBrew:
     @pytest.mark.trio
     async def test_cask_version(self, brew: Brew):
         assert "4.12" <= await brew.latest_version("homebrew/cask/docker") <= "99"
-
-    @pytest.mark.trio
-    async def test_font_version(
-        self, brew: Brew, brew_tap_fonts: None  # pylint:disable=unused-argument
-    ):
-        assert "6.2" <= await brew.latest_version("homebrew/cask-fonts/font-fira-code")
 
     @pytest.mark.trio
     async def test_not_existing_formula(self, brew: Brew):
