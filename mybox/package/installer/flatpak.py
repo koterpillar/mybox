825769da8e9d0a6ffbab8747a131214b37bf50c6
Alexey Kotlyarov <a@koterpillar.com>
2025-02-01 22:02:55 +1100

    feat: Flatpak installer

---
+++ mybox/package/installer/flatpak.py
@@ -0,0 +1,52 @@
+from typing import Optional
+
+from .base import PackageCacheInstaller, PackageVersionInfo
+
+
+class Flatpak(PackageCacheInstaller[PackageVersionInfo]):
+    async def install(self, package: str) -> None:
+        await self.driver.run("flatpak", "install", "-y", package)
+        await super().install(package)
+
+    # can't install an old version of a package in tests
+    async def upgrade(self, package: str) -> None:  # pragma: no cover
+        await self.driver.run("flatpak", "upgrade", "-y", package)
+        await super().upgrade(package)
+
+    @staticmethod
+    def parse_versions(output: str) -> dict[str, str]:
+        versions = {}
+        for line in output.splitlines():
+            name, version = line.split()
+            versions[name] = version
+        return versions
+
+    async def get_installed(self) -> dict[str, str]:
+        result = await self.driver.run_output(
+            "flatpak", "list", "--app", "--columns=application,version"
+        )
+        return self.parse_versions(result)
+
+    async def get_latest(self) -> dict[str, str]:
+        result = await self.driver.run_output(
+            "flatpak", "remote-ls", "--app", "--columns=application,version"
+        )
+        return self.parse_versions(result)
+
+    async def get_package_info(
+        self, package: Optional[str]
+    ) -> dict[str, PackageVersionInfo]:
+        if package is not None:
+            # Getting all package versions is complete, no package-specific
+            # queries needed
+            return dict()
+
+        latest = await self.get_latest()
+        installed = await self.get_installed()
+
+        return {
+            name: PackageVersionInfo(
+                installed=installed.get(name), latest=latest_version
+            )
+            for name, latest_version in latest.items()
+        }
