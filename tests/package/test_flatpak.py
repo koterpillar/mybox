bcb827698ba06a48f6b441ea54836802e71fd618
Alexey Kotlyarov <a@koterpillar.com>
2025-03-29 10:01:25 +1100

    feat: Flatpak package

---
+++ tests/package/test_flatpak.py
@@ -0,0 +1,22 @@
+import pytest
+
+from .base import PackageArgs, PackageTestBase
+
+
+class TestFlatpak(PackageTestBase):
+    affects_system = True
+
+    async def constructor_args(self) -> PackageArgs:
+        return {
+            "flatpak": "org.videolan.VLC",
+        }
+
+    async def check_applicable(self) -> None:
+        await super().check_applicable()
+        if not (await self.driver.os()).switch(linux=True, macos=False):
+            pytest.skip("This test is only applicable on Linux.")
+
+    async def check_installed_command(self):
+        return ["vlc", "--version"]
+
+    check_installed_output = "NVIM"
