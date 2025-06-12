e61fa48df0257554a2bae1a5d85c30ad80e5d59d
Alexey Kotlyarov <a@koterpillar.com>
2025-06-12 20:11:05 +1000

    chore(haskell): use DOCKER_IMAGE to test

---
+++ test/Mybox/Driver/IOSpec.hs
@@ -0,0 +1,46 @@
+module Mybox.Driver.IOSpec where
+
+import           Control.Monad.Reader (runReaderT)
+
+import           Data.List.NonEmpty   (NonEmpty (..))
+
+import qualified Data.Text            as Text
+
+import           Test.Hspec
+
+import           Mybox.Driver.Class
+import           Mybox.Driver.IO
+
+import           System.Environment
+import           System.Exit          (ExitCode (..))
+
+testDriver :: IO IODriver
+testDriver = do
+  image_ <- lookupEnv "DOCKER_IMAGE"
+  case image_ of
+    Nothing    -> pure localDriver
+    Just image -> dockerDriver (Text.pack image)
+
+run :: IODriver -> (forall m. MonadDriver m => m a) -> IO a
+run drv action = runReaderT action drv
+
+spec :: Spec
+spec =
+  before testDriver $ do
+    it "returns ExitSuccess for true command" $ \drv -> do
+      result <- run drv $ drvRunOk $ "true" :| []
+      result `shouldBe` ExitSuccess
+    it "returns ExitFailure for false command" $ \drv -> do
+      result <- run drv $ drvRunOk $ "false" :| []
+      result `shouldSatisfy` \case
+        ExitSuccess -> False
+        ExitFailure _ -> True
+    it "captures output from echo command" $ \drv -> do
+      result <- run drv $ drvRunOutput $ "echo" :| ["hello", "world"]
+      result `shouldBe` "hello world"
+    it "handles empty output correctly" $ \drv -> do
+      result <- run drv $ drvRunOutput $ "printf" :| [""]
+      result `shouldBe` ""
+    it "trims whitespace from output" $ \drv -> do
+      result <- run drv $ drvRunOutput $ "echo" :| ["  trimmed  "]
+      result `shouldBe` "trimmed"
