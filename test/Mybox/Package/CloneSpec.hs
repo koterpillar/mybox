5cd149d54db47eb3f9fdbc3ae4539cb78fe73f68
Alexey Kotlyarov <a@koterpillar.com>
2025-06-15 09:21:01 +1000

    chore(haskell): test clone to existing directory

--- test/Mybox/Package/CloneSpec.hs
+++ test/Mybox/Package/CloneSpec.hs
@@ -15,17 +15,25 @@ spec = do
               ("cat" :| [psaDirectory <> "/templates/zshrc.zsh-template"])
               "alias ohmyzsh"
   packageSpec baseClone
-  let checkoutEarlierCommit :: MonadDriver m => Text -> m ()
-      checkoutEarlierCommit dir = do
-        drvRun
-          $ "git" :| ["clone", "https://github.com/ohmyzsh/ohmyzsh.git", dir]
-        drvRun $ "git" :| ["-C", dir, "checkout", "HEAD~"]
   packageSpec $ \psa ->
     baseClone psa
       & psName "branch switch"
-      & psPreinstall (checkoutEarlierCommit (psaDirectory psa))
+      & psPreinstall (checkoutEarlierCommit psa)
+  packageSpec $ \psa ->
+    baseClone psa
+      & psName "empty directory"
+      & psPreinstall (createEmptyDirectory psa)
   packageSpec $ \PackageSpecArgs {..} ->
     ps (ClonePackage "node-fetch/node-fetch" (Just "2.x") psaDirectory)
       & psCheckInstalledCommandOutput
           ("cat" :| [psaDirectory <> "/package.json"])
           "\"version\": \"2"
+
+createEmptyDirectory :: MonadDriver m => PackageSpecArgs -> m ()
+createEmptyDirectory PackageSpecArgs {..} = drvMkdir psaDirectory
+
+checkoutEarlierCommit :: MonadDriver m => PackageSpecArgs -> m ()
+checkoutEarlierCommit PackageSpecArgs {..} = do
+  drvRun
+    $ "git" :| ["clone", "https://github.com/ohmyzsh/ohmyzsh.git", psaDirectory]
+  drvRun $ "git" :| ["-C", psaDirectory, "checkout", "HEAD~"]
