b58beae7ed9f5c2ea1d61a60c5c27da444e48bbd
Alexey Kotlyarov <a@koterpillar.com>
2025-06-14 22:37:14 +1000

    chore(haskell): test for clone

---
+++ test/Mybox/SpecBase.hs
@@ -0,0 +1,21 @@
+module Mybox.SpecBase where
+
+import           Control.Monad.IO.Class (MonadIO)
+import           Control.Monad.Reader   (runReaderT)
+
+import qualified Data.Text              as Text
+
+import           Mybox.Driver.Class
+import           Mybox.Driver.IO
+
+import           System.Environment
+
+withDriver :: (IODriver -> IO ()) -> IO ()
+withDriver act = do
+  image_ <- lookupEnv "DOCKER_IMAGE"
+  case image_ of
+    Nothing    -> testHostDriver act
+    Just image -> dockerDriver (Text.pack image) act
+
+run :: IODriver -> (forall m. (MonadDriver m, MonadIO m) => m a) -> IO a
+run drv action = runReaderT action drv
