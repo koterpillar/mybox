92d6bc2aa1c462782207fb631d8c960096032319
Alexey Kotlyarov <a@koterpillar.com>
2025-07-03 22:07:02 +1000

    chore(haskell): Pipx package

--- src/Mybox/Aeson.hs
+++ src/Mybox/Aeson.hs
@@ -1,5 +1,6 @@
 module Mybox.Aeson (jsonEncode, jsonDecode) where
 
+import Control.Exception.Safe (MonadThrow, throwString)
 import Data.Aeson
 import Data.ByteString.Lazy qualified as LBS
 import Data.Text (Text)
@@ -8,5 +9,7 @@ import Data.Text.Encoding qualified as Text
 jsonEncode :: ToJSON a => a -> Text
 jsonEncode = Text.decodeUtf8 . LBS.toStrict . encode
 
-jsonDecode :: FromJSON a => Text -> Maybe a
-jsonDecode = decodeStrictText
+jsonDecode :: (FromJSON a, MonadThrow m) => String -> Text -> m a
+jsonDecode desc v = case eitherDecodeStrictText v of
+  Left err -> throwString $ "Failed to decode " <> desc <> ": " <> err
+  Right a -> pure a
---
+++ test/Mybox/Package/PipxSpec.hs
@@ -0,0 +1,36 @@
+module Mybox.Package.PipxSpec where
+
+import Data.Text qualified as Text
+
+import Mybox.Package.Class
+import Mybox.Package.Pipx
+import Mybox.Package.SpecBase
+import Mybox.Prelude
+import Mybox.SpecBase
+
+spec :: Spec
+spec = do
+  describe "remote version" $ do
+    around withTestEnv $ do
+      it "gets version for existing package" $ do
+        let package = PipxPackage "black"
+        version <- remoteVersion package
+        version `shouldSatisfy` (>= "25.0.0")
+        version `shouldSatisfy` (<= "999.0.0")
+      it "fails for non-existent package" $ do
+        let package = PipxPackage "xxxxxxxxxxxx"
+        remoteVersion package `shouldThrow` anyException
+      it "returns a Git commit hash for a git package" $ do
+        let package = PipxPackage "git+https://github.com/django/django.git"
+        remoteVersion package >>= (`shouldSatisfy` (\v -> Text.length v == 40))
+  let tqdmPackage name _ =
+        ps (PipxPackage name)
+          & checkInstalledCommandOutput
+            ("tqdm" :| ["--help"])
+            "Usage:\n  tqdm"
+          & ignorePath ".shiv"
+          & ignorePath ".local/bin/pipx"
+          & ignorePath ".local/share/pipx"
+          & ignorePath ".local/state/pipx/log"
+  packageSpec $ tqdmPackage "tqdm"
+  packageSpec $ tqdmPackage "git+https://github.com/tqdm/tqdm.git"
