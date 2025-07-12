3b52e4bd32270b4246f4ae81e416321b8b25df43
Alexey Kotlyarov <a@koterpillar.com>
2025-07-12 22:13:48 +1000

    chore(haskell): fix Flatpak prerequisite test

--- test/Mybox/Installer/AptSpec.hs
+++ test/Mybox/Installer/AptSpec.hs
@@ -5,9 +5,5 @@ import Mybox.Installer.Apt
 import Mybox.Installer.SpecBase
 import Mybox.SpecBase
 
-debianLike :: OS -> Bool
-debianLike (Linux (Debian _)) = True
-debianLike _ = False
-
 spec :: Spec
-spec = onlyIf (debianLike <$> drvOS) $ installerSpec apt
+spec = onlyIfOS (\case Linux (Debian _) -> True; _ -> False) $ installerSpec apt
--- test/Mybox/Installer/FlatpakSpec.hs
+++ test/Mybox/Installer/FlatpakSpec.hs
@@ -12,20 +12,10 @@ import Mybox.Prelude
 import Mybox.SpecBase
 import Mybox.Tracker
 
-linux :: OS -> Bool
-linux (Linux _) = True
-linux _ = False
-
-prerequisites :: Driver :> es => Eff es Bool
-prerequisites = do
-  os <- drvOS
-  case os of
-    Linux _ -> do
-      docker <- drvRunOutputExit $ shellRaw "echo $DOCKER_IMAGE"
-      case docker.output of
-        "" -> pure False
-        _ -> pure True
-    _ -> pure False
+inDocker :: Driver :> es => Eff es Bool
+inDocker = do
+  r <- drvRunOutputExit (shellRaw "echo $DOCKER")
+  pure $ r.output /= ""
 
 expectedFlatpakVersion :: Text -> Bool
 expectedFlatpakVersion version = case Text.splitOn ":" version of
@@ -33,12 +23,13 @@ expectedFlatpakVersion version = case Text.splitOn ":" version of
   _ -> False
 
 spec :: Spec
-spec = onlyIf prerequisites $
-  around withTestEnv $ do
-    describe "flatpak" $
-      beforeAll_ (nullTrackerSession $ runInstallQueue $ install flatpakPackage) $ do
-        describe "iLatestVersion" $ do
-          it "returns valid version for an existing package" $ do
-            iLatestVersion flatpak "org.gnome.Shotwell" >>= (`shouldSatisfy` expectedFlatpakVersion)
-          it "fails for non-existent package" $ do
-            iLatestVersion flatpak "org.gnome.Shotwell.NonExistent" `shouldThrow` anyException
+spec = onlyIfOS (\case Linux _ -> True; _ -> False) $
+  skipIf inDocker $
+    around withTestEnv $ do
+      describe "flatpak" $
+        beforeAll_ (nullTrackerSession $ runInstallQueue $ ensureInstalled flatpakPackage) $ do
+          describe "iLatestVersion" $ do
+            it "returns valid version for an existing package" $ do
+              iLatestVersion flatpak "org.gnome.Shotwell" >>= (`shouldSatisfy` expectedFlatpakVersion)
+            it "fails for non-existent package" $ do
+              iLatestVersion flatpak "org.gnome.Shotwell.NonExistent" `shouldThrow` anyException
