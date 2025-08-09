68348718df722a74a612388e4e355e7d4854c9c3
Alexey Kotlyarov <a@koterpillar.com>
2025-08-10 09:53:59 +1000

    chore(haskell): refactor architecture parsing

--- src/Mybox/Platform.hs
+++ src/Mybox/Platform.hs
@@ -1,34 +1,59 @@
 module Mybox.Platform (
   Architecture (..),
+  architectureString,
+  parseArchitecture,
   architectureFilters,
   OS (..),
   Distribution (..),
   osFilters,
 ) where
 
+import Data.List.NonEmpty qualified as NonEmpty
 import Data.Map.Strict qualified as Map
+import Data.Text qualified as Text
 
+import Mybox.Aeson
 import Mybox.Filters
 import Mybox.Prelude
 
+findKey :: (Eq a, Foldable t) => Map b (t a) -> a -> Maybe b
+findKey m s = fst <$> find (elem s . snd) (Map.toList m)
+
 data Architecture = X86_64 | Aarch64 deriving (Eq, Ord, Show)
 
-archSynonyms :: Map Text [Text]
-archSynonyms =
+archStrings :: Map Architecture (NonEmpty Text)
+archStrings =
   Map.fromList
-    [ ("aarch64", ["aarch64", "arm64", "arm"])
-    , ("i386", ["i386", "i686", "x86"])
-    , ("mips", [])
-    , ("powerpc", ["ppc"])
-    , ("s390x", [])
-    , ("x86_64", ["x86_64", "amd64", "x64"])
+    [ (X86_64, "x86_64" :| ["amd64", "x64"])
+    , (Aarch64, "aarch64" :| ["arm64", "arm"])
     ]
 
+architectureString :: Architecture -> Text
+architectureString = NonEmpty.head . fromJust . flip Map.lookup archStrings
+
+parseArchitecture :: Text -> Either String Architecture
+parseArchitecture a =
+  maybe (Left $ "Unsupported architecture: " <> Text.unpack a) pure $ findKey archStrings a
+
+allArchSynonyms :: Map Text [Text]
+allArchSynonyms =
+  Map.fromList $
+    map (\(a :| as) -> (a, a : as)) (toList archStrings)
+      <> [ ("i386", ["i386", "i686", "x86"])
+         , ("mips", [])
+         , ("powerpc", ["ppc"])
+         , ("s390x", [])
+         ]
+
+instance ToJSON Architecture where
+  toJSON = String . architectureString
+
+instance FromJSON Architecture where
+  parseJSON (String s) = either fail pure $ parseArchitecture s
+  parseJSON _ = fail "Expected a string for Architecture"
+
 architectureFilters :: Architecture -> [Text -> Bool]
-architectureFilters = fromSynonyms archSynonyms . akey
- where
-  akey X86_64 = "x86_64"
-  akey Aarch64 = "aarch64"
+architectureFilters = fromSynonyms allArchSynonyms . architectureString
 
 data Distribution = Debian {variant :: Text} | Fedora deriving (Eq, Ord, Show)
 
