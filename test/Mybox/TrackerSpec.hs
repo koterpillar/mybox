7179f77116494bd31869b89bc7bea4be75ed22c9
Alexey Kotlyarov <a@koterpillar.com>
2025-08-19 22:25:01 +1000

    chore(haskell): test real tracker implementation

--- test/Mybox/TrackerSpec.hs
+++ test/Mybox/TrackerSpec.hs
@@ -2,6 +2,7 @@ module Mybox.TrackerSpec where
 
 import Data.Set qualified as Set
 
+import Mybox.Driver
 import Mybox.Prelude
 import Mybox.SpecBase
 import Mybox.Tracker
@@ -43,3 +44,21 @@ spec = do
               ["common-file", "pkg3-file-new"]
         )
     deleted `shouldBe` Set.fromList (map mkAbs ["pkg3-file", "pkg4-file"])
+  describe "drvTracker" $ do
+    it "gets, sets state and removes files" $
+      drvTempDir $ \dir -> do
+        let testFile = dir </> "file1"
+        drvWriteFile testFile "content"
+        let state = dir </> "state.json"
+        let pkg = DummyPackage "pkg1"
+        -- Record the test file as owned by the package
+        drvTracker state $ trkSession $ trkAdd pkg testFile
+        -- State should be written out
+        drvIsFile state >>= (`shouldBe` True)
+        -- Now record no files
+        drvTracker state $ trkSession $ pure ()
+        -- State is still there but empty
+        drvIsFile state >>= (`shouldBe` True)
+        drvReadFile state >>= (`shouldBe` "{\"trackedFiles\":[]}")
+        -- Test file should be deleted
+        drvIsFile testFile >>= (`shouldBe` False)
