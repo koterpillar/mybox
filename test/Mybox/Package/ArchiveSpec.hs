01aad9c8005206f2ead646965ef42820c09e5ba0
Alexey Kotlyarov <a@koterpillar.com>
2025-08-20 15:19:53 +1000

    chore(haskell): more tests for archive

--- test/Mybox/Package/ArchiveSpec.hs
+++ test/Mybox/Package/ArchiveSpec.hs
@@ -23,3 +23,18 @@ spec = do
       findBinary pkg "test-binary" >>= (`shouldBe` bin)
     it "errors when binary does not exist" $ do
       findBinary pkg "test-binary" `shouldThrow` anyErrorCall
+  describe "withExtensions" $ do
+    let extensions = ["txt", "md"]
+    it "returns the same extension if already given" $
+      withExtensions extensions "file.txt" `shouldBe` ["file.txt"]
+    it "adds all extensions if not given" $
+      withExtensions extensions "file" `shouldBe` ["file.txt", "file.md"]
+    it "errors on multiple extensions" $ do
+      evaluate (withExtensions extensions "file.txt.md") `shouldThrow` anyErrorCall
+  describe "iconPath" $ do
+    it "returns correct path for SVG" $
+      iconPath (mkPath @AnyAnchor "icon.svg") `shouldBe` mkPath "hicolor/scalable/apps/icon.svg"
+    it "returns correct path for PNG" $ do
+      iconPath (mkPath @AnyAnchor "icon.png") `shouldBe` mkPath "hicolor/16x16/apps/icon.png"
+    it "returns path with resolution for PNG" $ do
+      iconPath (mkPath @AnyAnchor "somewhere/32x32/icon.png") `shouldBe` mkPath "hicolor/32x32/apps/icon.png"
