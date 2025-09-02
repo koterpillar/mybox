512aac34b351c5d82faa2c5ca062ae0d0905595f
Alexey Kotlyarov <a@koterpillar.com>
2025-09-02 18:54:01 +1000

    chore(haskell): move uncovered files to a separate directory

--- test/Mybox/Spec/Utils.hs
+++ test/Mybox/Spec/Utils.hs
@@ -1,6 +1,5 @@
-module Mybox.Spec.Utils where
+module Mybox.Spec.Utils (
+  module Mybox.Spec.Uncovered.Utils,
+) where
 
-import Mybox.Prelude
-
-requireJust :: String -> Maybe a -> a
-requireJust !msg = fromMaybe (error msg)
+import Mybox.Spec.Uncovered.Utils
