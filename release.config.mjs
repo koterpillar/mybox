15216e4d7891ef4326a2977902f2835a9f3a3d83
Alexey Kotlyarov <a@koterpillar.com>
2025-10-08 21:48:12 +1100

    fix: remove tag clashes when building

---
+++ release.config.mjs
@@ -0,0 +1,35 @@
+const isBuild = process.env.GITHUB_JOB === "build";
+
+export default {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    ...(isBuild
+      ? [
+          // Build job; only update the version in package.yaml
+          [
+            "semantic-release-replace-plugin",
+            {
+              replacements: [
+                {
+                  files: ["package.yaml"],
+                  from: "^version: 0\\.0\\.0$",
+                  to: "version: ${nextRelease.version}",
+                },
+              ],
+            },
+          ],
+        ]
+      : [
+          // Actual release; the build jobs must have run before and produced
+          // the executables
+          "@semantic-release/release-notes-generator",
+          [
+            "@semantic-release/github",
+            {
+              assets: [{ path: "bootstrap" }, { path: "executables/*" }],
+            },
+          ],
+        ]),
+  ],
+};
