6b4db9713f0eddc96d2a0658fc17a8de6419c942
Alexey Kotlyarov <a@koterpillar.com>
2025-10-12 19:20:27 +1100

    chore: single var for driver stats

--- test/Mybox/SpecHook.hs
+++ test/Mybox/SpecHook.hs
@@ -1,37 +1,24 @@
 module Mybox.SpecHook where
 
-import Control.Concurrent.MVar
-import Data.List (sortOn)
 import Data.Map qualified as Map
-import Data.Ord (Down (..))
-import Data.Text qualified as Text
 import Test.Hspec qualified as Hspec
 
 import Mybox.Display.None
-import Mybox.Driver
 import Mybox.Driver.Stats
 import Mybox.Driver.Test
-import Mybox.Prelude hiding (MVar, modifyMVar_, newMVar, takeMVar)
+import Mybox.Prelude
 import Mybox.SpecBase
 import Mybox.Stores
 
 hook :: Spec -> Hspec.Spec
 hook spec = do
-  globalStats <- runIO $ newMVar Map.empty
-  afterAll_ (takeMVar globalStats >>= printStats 20) $ effSpec (dispatch globalStats) spec
+  statVar <- runIO $ runEff $ runConcurrent $ newMVar Map.empty
+  afterAll_ (runEff $ runConcurrent $ printStats statVar 20) $ effSpec (dispatch statVar) spec
 
-dispatch :: MVar (Map Args Int) -> Eff BaseEff r -> Eff '[IOE] r
-dispatch globalStats act =
+dispatch :: MVar DriverStats -> Eff BaseEff r -> Eff '[IOE] r
+dispatch statVar act =
   runConcurrent $
     noDisplay $
-      runStores $ do
-        (stats, r) <- testDriver $ driverStats act
-        liftIO $ modifyMVar_ globalStats $ pure . Map.unionWith (+) stats
-        pure r
-
-printStats :: Int -> Map Args Int -> IO ()
-printStats n stats = do
-  let top = take n $ sortOn (Down . snd) $ Map.toList stats
-  putStrLn $ "Top " <> show n <> " commands:"
-  forM_ top $ \(arg, count) ->
-    putStrLn $ Text.unpack (shellJoin arg) <> ": " <> show count
+      runStores $
+        testDriver $
+          driverStats statVar act
