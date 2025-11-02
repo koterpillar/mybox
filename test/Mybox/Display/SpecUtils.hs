fb8ed4830a7e58f1c596b14369f633e2df4903c9
Alexey Kotlyarov <a@koterpillar.com>
2025-11-03 09:20:50 +1100

    chore: runSimpleDisplayPure

--- test/Mybox/Display/SpecUtils.hs
+++ test/Mybox/Display/SpecUtils.hs
@@ -3,6 +3,10 @@ module Mybox.Display.SpecUtils where
 import Data.Text qualified as Text
 import System.IO
 
+import Mybox.Display.Class
+import Mybox.Display.Print (Print)
+import Mybox.Display.Print qualified as Print
+import Mybox.Display.Simple
 import Mybox.Driver
 import Mybox.Prelude
 
@@ -16,3 +20,9 @@ writeHandle act = localDriver $ drvTempFile $ \filePath -> do
       (inject . act)
   contents <- liftIO $ readFile fileName
   pure (r, contents)
+
+runSimpleDisplayPure :: forall a es r. ANSIDisplayable a => Eff (Display a : es) r -> Eff es (r, String)
+runSimpleDisplayPure act =
+  Print.runPure $
+    runSimpleDisplay $
+      inject @_ @(Display a : Print : _) act
