df5a5d117ee34808b32da26bccd7938f11e079b7
Alexey Kotlyarov <a@koterpillar.com>
2025-11-23 10:24:09 +1100

    chore: more tests for BrewRepo

--- test/Mybox/Package/BrewRepoSpec.hs
+++ test/Mybox/Package/BrewRepoSpec.hs
@@ -1,9 +1,12 @@
 module Mybox.Package.BrewRepoSpec where
 
 import Mybox.Package.BrewRepo
+import Mybox.Package.Class
+import Mybox.Package.Queue
 import Mybox.Package.SpecBase
 import Mybox.Prelude
 import Mybox.SpecBase
+import Mybox.Tracker
 
 spec :: Spec
 spec = do
@@ -11,7 +14,11 @@ spec = do
     @BrewRepo
     [ (Nothing, "{\"brew_tap\": \"easysoft/tap\"}")
     ]
-  onlyIf "Homebrew repository tests require virtual system (Docker or CI)" virtualSystem $
+  onlyIf "Homebrew repository tests require virtual system (Docker or CI)" virtualSystem $ do
+    withEff (nullTracker . runInstallQueue) $ do
+      it "reports not installed when not tapped" $ do
+        enableSudo
+        localVersion (mkBrewRepo "denji/nginx") >>= (`shouldBe` Nothing)
     packageSpec $
       ps (mkBrewRepo "easysoft/tap")
         & preinstallEnableSudo
