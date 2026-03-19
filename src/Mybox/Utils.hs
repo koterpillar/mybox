49b1146aa3342d715b8d8649cebb3fefde5def59
Alexey Kotlyarov <a@koterpillar.com>
2026-03-19 16:11:09 +1100

    fix: support full Git URLs for Brew repositories

--- src/Mybox/Utils.hs
+++ src/Mybox/Utils.hs
@@ -6,3 +6,14 @@ import System.FilePath.Glob (compile, match)
 
 glob :: Text -> Text -> Bool
 glob pattern text = match (compile $ Text.unpack pattern) (Text.unpack text)
+
+-- | Check if a repository URL is a GitHub shortcut (e.g., "user/repo").
+isGithubShortcut :: Text -> Bool
+isGithubShortcut r =
+  not (Text.isPrefixOf "https://" r) && not (Text.isInfixOf "@" r)
+
+-- | Normalize GitHub shortcuts to full URLs.
+normalizeGitRepoUrl :: Text -> Text
+normalizeGitRepoUrl r
+  | isGithubShortcut r = "https://github.com/" <> r <> ".git"
+  | otherwise = r
