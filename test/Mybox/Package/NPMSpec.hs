6d02c33306b6ef5dea6275a698e111808f08b679
Alexey Kotlyarov <a@koterpillar.com>
2026-07-05 09:18:19 +1000

    fix: more tests for npm

--- test/Mybox/Package/NPMSpec.hs
+++ test/Mybox/Package/NPMSpec.hs
@@ -1,10 +1,14 @@
 module Mybox.Package.NPMSpec where
 
+import Data.Map qualified as Map
+
+import Mybox.Aeson
 import Mybox.Package.Class
-import Mybox.Package.NPM
+import Mybox.Package.NPM.Internal
 import Mybox.Package.Queue
 import Mybox.Package.SpecBase
 import Mybox.Prelude
+import Mybox.Spec.Utils
 import Mybox.SpecBase
 import Mybox.Tracker
 
@@ -30,3 +34,16 @@ spec = do
           ("express" :| ["--help"])
           "engine support"
         & ignorePaths [".npm" </> "_cacache", ".npm" </> "_logs", ".npm" </> "_update-notifier-last-checked"]
+  describe "PackageJson" $ do
+    it "parses bin field" $ do
+      let json = "{\"bin\": {\"foo\": \"bar\"}}"
+      let resultE = jsonDecode "example" json
+      resultE `shouldSatisfy` isRight
+      let pkg = requireRight resultE
+      pkg `shouldBe` PackageJson (Map.singleton "foo" "bar")
+    it "parses empty object" $ do
+      let json = "{}"
+      let resultE = jsonDecode "example" json
+      resultE `shouldSatisfy` isRight
+      let pkg = requireRight resultE
+      pkg `shouldBe` PackageJson Map.empty
